6f3a7bf13f8258ab8843d6c8e7366d7a92d84a2d
joudialkabbani <joudi.kab01@gmail.com>
2022-09-27 22:29:28 +0300

    .....

--- lib/data/repository/characters_repository.dart
+++ lib/data/repository/characters_repository.dart
@@ -4,7 +4,7 @@ import '../models/characters.dart';
 class CharactersRepository {
   final CharactersWebServices charactersWebServices;
   CharactersRepository(this.charactersWebServices);
-  Future<List<dynamic>> getAllCharacters() async {
+  Future<List<Character>> getAllCharacters() async {
     final characters = await charactersWebServices.getAllCharacters();
     return characters
         .map((character) => Character.fromJson(character))
